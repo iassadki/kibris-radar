4d23f3f9836b811dc288e7ad4c706a4f82405546
Elyas Belaidi <e.belaidi@commandstrategy.com>
2025-01-29 10:24:14 +0100

    push

--- backend/backend.js
+++ backend/backend.js
@@ -1,17 +1,89 @@
-const mqtt = require('mqtt')
-const client = mqtt.connect('mqtt://test.mosquitto.org')
-client.on('connect', function () {
-    console.log('test1')
-    client.subscribe('13A2004147961D', function (err) {
-        console.log('test2')
-        if (!err){
-            console.log('test3')
-            client.publish('13A2004147961D', 'Hello mqtt')
-        }
-    })
-})
-client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(topic, message.toString())
-    client.end()
-})
+var {SerialPort} = require('serialport');
+var xbee_api = require('xbee-api');
+var C = xbee_api.constants;
+require('dotenv').config()
+
+if (!process.env.SERIAL_PORT)
+  throw new Error('Missing SERIAL_PORT environment variable');
+
+if (!process.env.SERIAL_BAUDRATE)
+  throw new Error('Missing SERIAL_BAUDRATE environment variable');
+
+// Replace with your serial port and baud rate (9600 by default)
+const SERIAL_PORT = process.env.SERIAL_PORT;
+
+// Ensure to configure your XBEE Module in API MODE 2
+var xbeeAPI = new xbee_api.XBeeAPI({
+  api_mode: 1
+});
+
+let serialport = new SerialPort(SERIAL_PORT, {
+  baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
+}, function (err) {
+  if (err) {
+    return console.log('Creating SerialPort', err.message)
+  }
+});
+
+serialport.pipe(xbeeAPI.parser);
+xbeeAPI.builder.pipe(serialport);
+
+const BROADCAST_ADDRESS = "FFFFFFFFFFFFFFFF";
+serialport.on("open", function () {
+
+  //Sample local command to ask local Xbee module the value of NODE IDENTIFIER
+  var frame_obj = { // AT Request to be sent
+    type: C.FRAME_TYPE.AT_COMMAND,
+    command: "NI",
+    commandParameter: [],
+  };
+
+  xbeeAPI.builder.write(frame_obj);
+
+  //Sample remote command to ask all remote Xbee modules the value of their NODE IDENTIFIER
+  frame_obj = { // AT Request to be sent
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination64: BROADCAST_ADDRESS,
+    command: "NI",
+    commandParameter: [],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+});
+
+// All frames parsed by the XBee will be emitted here
+xbeeAPI.parser.on("data", function (frame) {
+
+  //on new device is joined, register it
+  if (C.FRAME_TYPE.JOIN_NOTIFICATION_STATUS === frame.type) {
+    console.log("New device has joined network, you can register has new device available");
+
+  }
+
+  if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
+    console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
+    let dataReceived = String.fromCharCode.apply(null, frame.data);
+    console.log(">> ZIGBEE_RECEIVE_PACKET >", dataReceived);
+
+  }
+
+  if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
+    // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
+    console.log("NODE_IDENTIFICATION");
+
+  } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
+
+    console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
+    console.log(frame)
+    console.log("Value of ADO can be retrieved with frame.analogSamples.AD0")
+    console.log(frame.analogSamples.AD0)
+
+  } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
+    console.log("REMOTE_COMMAND_RESPONSE")
+  } else {
+    console.debug(frame);
+    let dataReceived = String.fromCharCode.apply(null, frame.commandData)
+    console.log(dataReceived);
+  }
+
+});
